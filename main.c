854b5e76d54ac1e95df0ff38661d71cc33bf93e7
williamsimms <williamsimms193@gmail.com>
2021-02-24 14:10:40 -0500

    get

--- main.c
+++ main.c
@@ -12,14 +12,15 @@ typedef struct Node *Node;
 
 void insertFirst(Node *head, int data);
 void insertLast(Node *head, int data);
+void insertAt(Node *head, int data);
 void printList(Node);
 void deleteFirst(Node head);
 int length(Node);
-void insertAt(Node head, int data);
 bool deleteData(Node head, int data);
 void deleteLast(Node head);
 Node getFirst(Node head);
 Node getLast(Node head);
+Node getAt(Node head, int index);
 void findIndex(Node head, int data);
 bool contains(Node head, int data);
 bool isEmpty();
@@ -28,6 +29,8 @@ int main(void) {
   Node head = NULL;
 
   // insertFirst(&head,10)
+  // insertLast(&head,15)
+  // insertAt(&head,10,4)
 
   return 0;
 }
@@ -74,6 +77,80 @@ void insertLast(Node *head, int data) {
   current->next = newLastNode;
 }
 
+void insertAt(Node *head, int data, int index) {
+  Node current = *head;
+
+  if (current == NULL) {
+    Node newHeadNode;
+    newHeadNode = malloc(sizeof(struct Node));
+    newHeadNode->data = data;
+    newHeadNode->next = NULL;
+    *head = newHeadNode;
+    return;
+  }
+
+  int counter = 0;
+
+  //
+}
+
+Node getFirst(Node head) {
+  Node current = head;
+
+  if (current == NULL) {
+    puts("List is Empty.");
+    exit(EXIT_FAILURE);
+  }
+
+  return head;
+}
+
+Node getLast(Node head) {
+  Node current = head;
+
+  if (current == NULL) {
+    puts("List is Empty.");
+    exit(EXIT_FAILURE);
+  }
+
+  while (current != NULL) {
+    current = current->next;
+  }
+
+  return current;
+}
+
+Node getAt(Node head, int index) {
+  Node current = head;
+
+  if (current == NULL) {
+    puts("List is Empty.");
+    exit(EXIT_FAILURE);
+  }
+
+  if (index < 0) {
+    puts("Index can not be less than 0");
+    exit(EXIT_FAILURE);
+  }
+
+  if (index == 0) {
+    return head;
+  }
+
+  int counter = 0;
+
+  while (current != NULL) {
+    if (counter == index) {
+      break;
+    }
+
+    current = current->next;
+    counter++;
+  }
+
+  return current;
+}
+
 void printList(Node head) {
   Node currentNode = head;
 
@@ -87,6 +164,11 @@ void printList(Node head) {
 
 int length(Node head) {
   Node currentNode = head;
+
+  if (currentNode->next == NULL) {
+    return 0;
+  }
+
   int counter = 0;
 
   while (currentNode->next != NULL) {
